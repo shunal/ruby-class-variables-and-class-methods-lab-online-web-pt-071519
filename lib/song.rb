00161202a43cbb020e5458e97ca53be68514a9cd
Steven Hunal <stevehunal@gmail.com>
2019-08-01 23:30:32 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song 
+ @@count = 0 
+ @@artists = []
+ @@genres = []
+ @@genre_count = {}
+ @@artist_count = {}
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@count += 1 
+    @@genres << genre
+  end
+  
+  def self.genre_count
+    @@genres.each do |genre|
+      if @@genre_count.keys.include?(genre)
+        @@genre_count[genre] += 1
+      else 
+        @@genre_count[genre] = 1
+    end
+  end
+    @@genre_count
+  end 
+  
+  def self.artist_count
+    @@artists.each do |artists|
+      
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+
+end
